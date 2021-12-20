8589f1658b16e5bbcdf6658487e02bbd86b1cc73
HACKER <93648073+raihanvaliyakath@users.noreply.github.com>
2021-12-20 18:55:49 +0530

    Create transalation.py

---
+++ transalation.py
@@ -0,0 +1,37 @@
+
+START_TEXT = """ `Hai {}, 
+Am a YouTube Downloader Bot I Can Download Songs,Videos and Lyrics From YouTube and lyrics and  Would upload into Telegram. 
+Use /help Commands For More.` MAINTAINE BY © [RAIHAN](t.me/RAIHAN_TG)
+"""
+
+CMDS_TEXT = """
+`Here It is The List of Commamds and Its usage.`
+
+- /song - This Command is For Downloading Songs. 
+- /lyrics - This Command is For Scrapping Lyrics of a Song. 
+- /video - This Command is For Downloading Videos. 
+- Also You Can search videos via inline Mode on Bot. 
+
+`Exmples For Both Those Commands.`
+
+- /song [song name] or [youTube link]. 
+  [/song Alone]. 
+- /lyrics [song name]. 
+  [/lyrics alone] 
+- /video [video name] or [YouTube link] 
+  [/video Alone] 
+ 
+ABOUT_TEXT = """
+- **Bot :** `Song Downloader`
+- **Creator :** [RAIHAN](https://Github.com/raihanvaliyakath)
+- **Support :** [CLICK HERE](https://t.me/M_STER_TECH_GROUP)
+- **Source :** [CLICK HERE](https://github.com/raihanvaliyakath/SONG-BOT-V2)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
+
+
+
+
+
+DEV_DETAILS_TXT = [GITHUB](https://github.com/raihanvaliyakath) [TELEGRAM](t.me/RAIHAN_TG)
